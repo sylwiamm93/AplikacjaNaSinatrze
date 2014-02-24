61c8c21b76d0b559e1cff7c9694c32b94c8c9205
sylwiamm93 <sylwiamm93@gmail.com>
2014-02-24 20:08:06 +0100

    nowy formularz-nowy post

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'data_mapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Post
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+DataMapper.finalize
+Post.auto_upgrade!
 
 # akcja, route /
 get '/' do
@@ -25,10 +39,25 @@ get '/petla' do
 	erb :petla
 end
 
+
 get '/lista_postow' do
 	lista = { :imie => "Sylwia", :age => "20" }
 	haml :lista_postow, :locals => { :hash => lista }		
 end
+
+get '/posty' do
+	erb :posty
+end
+
+get '/posty/nowy' do
+	erb :nowy_post
+end
+
+post '/posty/utworz' do
+
+	erb :nowy_post
+end
+
 
 post '/oblicz' do
 a = params[:liczba1].to_i
