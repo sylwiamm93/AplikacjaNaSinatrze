07fba516c87f796d065e5d4cffe42d66fdc1341d
sylwiamm93 <sylwiamm93@gmail.com>
2014-04-28 20:25:49 +0200

    kategorie

--- public/javascript.js
+++ public/javascript.js
@@ -1,22 +1,73 @@
-function loadXMLDoc() {
-  var xmlhttp;
-if (window.XMLHttpRequest)
-  {
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
+$( document ).ready(function() {
 
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("ajax").innerHTML=xmlhttp.responseText;
-    }
-  }
+// publiczne/niepubliczne
 
-xmlhttp.open("GET","omnie2.haml",true);
-xmlhttp.send();
-}
+    $("a.publ").click(function(e){
+      e.preventDefault();
+      url = $(this).attr("href");
+      that = this
+      
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: {},
+        success: function(data, textStatus, jqXHR) {
+          console.log(data)
+          if (data.publiczny == 'tak') {
+            publiczny = 'niepubliczny'
+            nazwa_linka = "Uczyn niepublicznym"
+          } else {
+            publiczny = 'publiczny'
+            nazwa_linka = "Uczyn publicznym"
+          }
+          $(that).attr("href", '/' + data.id + '/post/' + publiczny);
+          $(that).html(nazwa_linka)
+        },
+        dataType: 'json'
+      });
+    });
+
+// usuwanie
+
+    $("a.del").click(function(e){
+      e.preventDefault();
+      url = $(this).attr("href");  
+      that = this
+
+      $.ajax({
+        type: "GET", 
+        url: url,
+        data: {},
+        success: function(data,textStatus, jqXHR){
+          $(that).closest("li").remove()
+        },
+        dataType: 'json'
+      });
+    });
+
+// komentarze
+
+    $("form button.kom").click(function(e){
+      e.preventDefault();
+      url = $(this).parent('form').attr("action");
+      // that = this
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: {
+          user: $('form #user').val(),
+          komentarz: $('form #kom').val(),
+          post_id: $('form #post_id').val()
+        },
+        success: function(data,textStatus,jqXHR){
+          $('#koment').append(data)
+        },
+        error: function ( jqXHR, textStatus, errorThrown) {
+          // console.log(jqXHR, textStatus, errorThrown)
+          // $('.errors').html(errorThrown)
+        },
+        dataType: 'html'
+      });
+    });
+});
+
--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'data_mapper'
+require 'pry'
+require 'json'
+require 'sinatra/partial'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
 
@@ -12,6 +15,7 @@ class Post
     property :publiczny, Text
     property :created_at, DateTime
     has n, :comments
+    belongs_to :category
 end
 
 class Comment
@@ -23,10 +27,23 @@ class Comment
     property :created_at, DateTime
 end
 
+class Category
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    has n, :posts
+end
 
 DataMapper.finalize
 Post.auto_upgrade!
 Comment.auto_upgrade!
+Category.auto_upgrade!
+
+
+if Category.count == 0
+  @cat = Category.create( :title=>"Wszystkie")
+end
+
 
 #set :layout_engine => :erb, :layout => :index
 # akcja, route /
@@ -41,88 +58,116 @@ get '/o-mnie' do
   haml :omnie
 end
 
-get '/hello/:name' do
-  # matches "GET /hello/foo" and "GET /hello/bar"
-  # params[:name] is 'foo' or 'bar'
-  "Hello #{params[:name]}!"
+
+
+#--------------------------------------------------------------
+                             #POSTY
+#tworzenie posta
+get '/posts/new' do
+  @cats = Category.all
+  haml :'posts/new'
 end
 
-get '/formularz' do
-	haml :formularz
+post '/posts/create' do
+ @cats = Category.all.get params[:category_id]
+ post = Post.create(:title=>params[:tytul], :body=>params[:tresc], :category_id=>params[:category_id])
+ redirect "/posts"
 end
 
-post '/oblicz' do
-a = params[:liczba1].to_i
-b = params[:liczba2].to_i
-(a*b).to_s
-end
-
-get '/petla' do
-	erb :petla
-end
-#---
-
-
-get '/nowy_post' do
-  haml :nowy_post
-end
-
-post '/dodaj' do
- post = Post.create(:title=>params[:tytul], :body=>params[:tresc])
- redirect "/lista_postow"
-end
-
-get '/lista_postow_publiczna' do 
-  @posts = Post.all(:publiczny => "tak")
-  haml :lista_postow_publiczna
-end
-
-post '/:id/lista_postow_update' do 
- @post = Post.get(params[:id])
- @post.update(:publiczny => "tak")
- redirect "/lista_postow_publiczna"
-end
-
-get '/lista_postow' do 
-  @posts = Post.all
-  haml :lista_postow
-end
-
-get '/:id/edit' do
-  @post = Post.get(params[:id])
-  haml :edit
-end
-
-post '/:id/update' do
-  @post = Post.get params[:id]
-  @post.update(params[:posty]) 
-  redirect "/lista_postow"
-end
-
-post '/:id/delete' do
-  @post = Post.get(params[:id])
-  @post.destroy
-  redirect "/lista_postow"
-end
-
-get '/:id/view' do
+#pojedynczy post
+get '/posts/:id' do
   @post = Post.get params[:id]
   @comments = @post.comments
-  haml :view
+  haml :'posts/show'
 end
+
+#wszystkie posty
+get '/posts' do 
+  # @cats = Category.all
+  @posts = Post.all
+  haml :'posts/post'
+end
+
+# uczyn posta publicznym
+post '/posts/:id/public' do 
+ @post = Post.get(params[:id])
+ @post.update(:publiczny => "tak")
+ content_type :json
+ { :id => @post.id, :publiczny => @post.publiczny}.to_json
+ 
+end
+
+
+# uczyn posta niepublicznym
+post '/posts/:id/notpublic' do 
+ @post = Post.get(params[:id])
+ @post.update(:publiczny => "nie")
+ content_type :json
+ { :id => @post.id, :publiczny => @post.publiczny }.to_json
+ # redirect "/lista_postow_publiczna"
+end
+
+# posty tylko publiczne
+get '/posts/public' do 
+  @posts = Post.all(:publiczny => "tak")
+  haml :'posts/show_public'
+end
+
+
+# akcje
+get '/posts/:id/edit' do
+  @post = Post.get(params[:id])
+  haml :'posts/edit'
+end
+
+post '/posts/:id/update' do
+  @post = Post.get params[:id]
+  @post.update(params[:posty]) 
+  redirect "/posts"
+end
+
+get '/posts/:id/destroy' do
+  @post = Post.get(params[:id])
+  @post.destroy
+  content_type :json
+  {:id => @post.id }.to_json
+  # binding.pry
+  # redirect "/lista_postow"
+end
+ 
+                          #COMMENTS
 
 post '/comment/create' do
   @post = Post.get params[:post_id]
-   Comment.create(:user=>params[:user], :body=>params[:komentarz], :post_id => params[:post_id])
-
-  #c = Comment.new
-  #c.user = params[:user]
-  #c.body = params[:body]
-  # c.post_id =
-  #c.post = @post
-  #c.save
-  redirect "/#{@post.id}/view"
-
-  # @post.comments << c
+  @c = Comment.create(:user=>params[:user], :body=>params[:komentarz], :post_id => params[:post_id])
+  content_type 'text/html', :charset => 'utf-8'
+  haml :'comments/show', :layout => false
 end
 
+                        #CATEGORIES
+
+get '/categories' do
+  @cats = Category.all
+  haml :'categories/category'
+end
+
+post '/categories/create' do
+  @c = Category.create(:title=>params[:nazwa])
+  content_type 'text/html', :charset => 'utf-8'
+  haml :'categories/list', :layout => false
+  redirect '/categories'
+end
+
+# pojedyncza kategoria
+get '/categories/:id/show' do
+  @cat = Category.get params[:id]
+  @posts = @cat.posts
+  haml :'categories/show'
+end
+
+# post '/post/:id/category_update' do
+#   @post = Post.get params[:post_id]
+#   @category = Category.get params[:id]
+#   @post.update( @post.category << @category)
+#   @post.save
+# end
